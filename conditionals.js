c1258d92abd19f095caaec3aa061c68dc44a1f75
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-09-08 23:37:16 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -13,6 +13,7 @@ if (cold) {
 console.log("Now you're ready to go outside!");
 */
 
+/*
 const temperature = 12;
 
 if (temperature < 0) {
@@ -24,3 +25,23 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+const temperature = -50;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+const raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
