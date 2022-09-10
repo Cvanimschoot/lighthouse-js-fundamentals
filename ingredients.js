857de311b5e5e60b573f31ef7aa3883b6d9d89c4
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-09-10 21:07:24 +0000

    Created and finished ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+console.log("Ingredients for banana bread:");
+let i = 0;
+while (i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+// Write a for loop that prints out the contents of ingredients:
+console.log("\nIngredients for banana bread:");
+for(let i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+let ingredientsReversed = ingredients.reverse();
+console.log("\nIngredients for banana bread:");
+for(let i = 0; i < ingredientsReversed.length; i++){
+  console.log(ingredientsReversed[i]);
+}
